dd21a3efdce19885d2ecca46ebbaf589f0d0d841
lenovo <820927710@qq.com>
2020-02-19 23:09:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 一个activity理解为一个页面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 这个方法是当activity第一次启动的时候执行
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
